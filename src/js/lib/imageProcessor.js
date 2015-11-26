a5584db3d58c2d29ac9e1c3ebb6a5eb14d5594cc
leon.shi <superRaytin@163.com>
2015-11-26 21:19:31 +0800

    image processor

--- src/js/lib/imageProcessor.js
+++ src/js/lib/imageProcessor.js
@@ -3,88 +3,210 @@
  */
 
 var window = global.window,
-    $ = window.$,
-    mass = global.mass,
-    cache = mass.cache,
-    _ = window._,
     document = window.document,
-    console = window.console,
-    alertify = window.alertify;
+    console = window.console;
 
-var ImageProcessor = {
-    draw: function(url, options, callback) {
-        options = options || {};
+var Buffer = global.Buffer;
+var fs = global.require('fs');
 
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
+// constructor
+function ImageProcessor(image, options) {
+    this.initialize(image, options);
+}
 
-        var image = new Image();
-        var qualityNum = options.quality || 0.92;
-        var imageType = options.imageType || 'image/jpeg';
+// init setter
+initSetter(ImageProcessor);
 
-        var sx = options.sx;
-        var sy = options.sy;
-        var sWidth = options.sWidth;
-        var sHeight = options.sHeight;
-        var dWidth = options.dWidth;
-        var dHeight = options.dHeight;
-        var dx = options.dx;
-        var dy = options.dy;
+// initialize
+ImageProcessor.prototype.initialize = function(image, options) {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
 
-        image.onload = function() {
-            canvas.width = sWidth;
-            canvas.height = sHeight;
+    image = image || this.originalImage;
 
-            ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+    var width = image.width;
+    var height = image.height;
+    var imageUrl = image.getAttribute('src');
 
-            var dataUrl = canvas.toDataURL(imageType, qualityNum);
+    options = options || {};
 
-            callback && callback(dataUrl);
-        };
+    canvas.width = width;
+    canvas.height = height;
 
-        image.src = url;
-    },
+    ctx.drawImage(image, 0, 0, width, height);
 
-    // Crop image
-    crop: function(targetCanvas, x, y, width, height, imageType, quality) {
+    this.canvas = canvas;
+    this.imageFormat = options.imageFormat || getImageFormat(imageUrl);
+    this.quality = options.quality || 0.92;
 
-        imageType = imageType || 'image/jpeg';
-        quality = quality || 0.92;
-
-        var targetctx = targetCanvas.getContext('2d');
-        var targetctxImageData = targetctx.getImageData(x, y, width, height);
-
-        var c = document.createElement('canvas');
-        var ctx = c.getContext('2d');
-
-        c.width = width;
-        c.height = height;
-
-        ctx.rect(0, 0, width, height);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.putImageData(targetctxImageData, 0, 0);
-
-        var dataUrl = c.toDataURL(imageType, quality);
-
-        c = ctx = null;
-
-        return dataUrl;
-    },
-
-    cleanArea: function(targetCanvas, x, y, width, height) {
-        // 获取目标画布的 context
-        var ctx = targetCanvas.getContext('2d');
-
-        // 清除像素
-        ctx.clearRect(x, y, width, height);
-
-        // 填充清除区域为白色背景
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(x, y, width, height);
-
-        return targetCanvas;
+    if (!this.originalImage) {
+        this.originalImage = image;
     }
+
+    return this;
 };
+
+// initialize
+ImageProcessor.prototype.reset = function() {
+    return this.initialize();
+};
+
+// draw image to canvas
+ImageProcessor.prototype.draw = function(url, options, callback) {
+    options = options || {};
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+
+    var image = new Image();
+    var qualityNum = options.quality || 0.92;
+    var imageType = options.imageType || 'image/jpeg';
+
+    var sx = options.sx;
+    var sy = options.sy;
+    var sWidth = options.sWidth;
+    var sHeight = options.sHeight;
+    var dWidth = options.dWidth;
+    var dHeight = options.dHeight;
+    var dx = options.dx;
+    var dy = options.dy;
+
+    image.onload = function() {
+        canvas.width = sWidth;
+        canvas.height = sHeight;
+
+        ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+
+        var dataUrl = canvas.toDataURL(imageType, qualityNum);
+
+        callback && callback(dataUrl);
+    };
+
+    image.src = url;
+};
+
+// crop image
+ImageProcessor.prototype.crop = function(x, y, width, height, callback) {
+    var imageFormat = this.imageFormat;
+    var quality = this.quality;
+
+    var canvas = this.canvas;
+    var ctx = canvas.getContext('2d');
+
+    // 取出要裁切的画布像素数据
+    var imageData = ctx.getImageData(x, y, width, height);
+
+    // 创建临时画布放置裁切出来的像素数据
+    var tempcanvas = document.createElement('canvas');
+    var tempctx = tempcanvas.getContext('2d');
+
+    tempcanvas.width = width;
+    tempcanvas.height = height;
+
+    tempctx.rect(0, 0, width, height);
+    tempctx.fillStyle = 'white';
+    tempctx.fill();
+    tempctx.putImageData(imageData, 0, 0);
+
+    var dataUrl = tempcanvas.toDataURL(imageFormat, quality);
+
+    tempcanvas = tempctx = null;
+
+    callback(dataUrl);
+};
+
+// convert dataurl to binary image file
+ImageProcessor.prototype.saveToImageFile = function(path, dataUrl, callback) {
+
+    if (arguments.length === 2) {
+        callback = dataUrl;
+        dataUrl = this.toDataUrl();
+    }
+
+    var imgbase64 = dataUrl.replace('data:image/jpeg;base64,', '')
+        .replace('data:image/jpg;base64,', '')
+        .replace('data:image/png;base64,', '');
+
+    var dataBuffer = new Buffer(imgbase64, 'base64');
+
+    // create image file
+    fs.writeFile(path, dataBuffer, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        callback();
+    });
+};
+
+// convert to data url
+ImageProcessor.prototype.toDataUrl = function(quality) {
+    quality = quality || this.quality;
+
+    var canvas = this.canvas;
+    var imageFormat = this.imageFormat;
+
+    return canvas.toDataURL(imageFormat, quality);
+};
+
+// clear canvas
+ImageProcessor.prototype.clear = function() {
+    var canvas = this.canvas;
+
+    this.cleanArea(0, 0, canvas.width, canvas.height);
+
+    return this;
+};
+
+ImageProcessor.prototype.cleanArea = function(x, y, width, height) {
+    var canvas = this.canvas;
+    // 获取目标画布的 context
+    var ctx = canvas.getContext('2d');
+
+    // 清除像素
+    ctx.clearRect(x, y, width, height);
+
+    // 填充清除区域为白色背景
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(x, y, width, height);
+
+    return this;
+};
+
+ImageProcessor.prototype.destroy = function() {
+    this.canvas = null;
+};
+
+// initialize setter
+function initSetter(construct) {
+    var keys = ['quality', 'imageFormat'];
+
+    for (var i = 0, l = keys.length; i < l; i++) {
+        var key = keys[i];
+
+        (function(k) {
+            construct.prototype['set' + upperCase(k)] = function(v) {
+                this[k] = v;
+            };
+        })(key);
+    }
+}
+
+// get image format
+function getImageFormat(str) {
+    var format = str.substr(str.lastIndexOf('.') + 1, str.length);
+
+    // 震惊：image/jpg 切出来的图片文件比 image/jpeg 大将近 10 倍！
+    format = format === 'jpg' ? 'jpeg' : format;
+
+    return 'image/' + format;
+}
+
+// uppercase first
+function upperCase(str) {
+    return str.replace(str.charAt(0), function(a) {
+        return a.toUpperCase();
+    });
+}
 
 module.exports = ImageProcessor;
