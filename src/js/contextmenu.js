6191b5e7ef938f6e6be1a9b0eabe616c2264dd98
leon.shi <superRaytin@163.com>
2015-11-27 15:52:58 +0800

    :curly_loop:

--- src/js/contextmenu.js
+++ src/js/contextmenu.js
@@ -7,13 +7,14 @@
 var window = global.window;
 var console = window.console;
 
+var gui = global.gui;
+
 var source = {};
 
 // 初始化右键菜单
 var contextMenuInit = function(){
     var mass = global.mass;
     var cache = mass.cache;
-    var gui = cache.gui;
 
     function menuMachine(name, option){
         return (source[name] = new gui.MenuItem(option));
