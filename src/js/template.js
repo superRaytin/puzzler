54282b17fa8be1fa1a8410fe9e6be4ed67876353
leon.shi <superRaytin@163.com>
2015-11-27 15:30:04 +0800

    update template

--- src/js/template.js
+++ src/js/template.js
@@ -6,36 +6,36 @@
 module.exports = {
     style:
         '<style type="text/css">\n' +
-            '\t.wid100{\n' +
+            '\t.wid100 {\n' +
                 '\t\twidth: 100%;\n' +
                 '\t\theight: auto;\n' +
                 '\t\tmargin-left: auto;\n' +
                 '\t\tmargin-right: auto;\n' +
             '\t}\n' +
-            '\t.ui-block-inner{\n' +
-                '\t\twidth: 990px;\n' +
+            '\t.ui-block-inner {\n' +
+                '\t\twidth: 100%;\n' +
                 '\t\theight: auto;\n' +
                 '\t\tmargin-left: auto;\n' +
                 '\t\tmargin-right: auto;\n' +
                 '\t\tposition: relative;\n' +
             '\t}\n' +
-            '\t.ui-rect{\n' +
+            '\t.ui-rect {\n' +
                 '\t\tposition: absolute;\n' +
-                '\t\tdisplay:inline-block;\n' +
+                '\t\tdisplay: inline-block;\n' +
                 '\t\tbackground: url("https://i.alipayobjects.com/e/201309/11htyhgbGn.gif") repeat;\n' +
             '\t}\n' +
-            '\t.ui-textarea{\n' +
+            '\t.ui-textarea {\n' +
                 '\t\tposition: absolute;\n' +
                 '\t\tfont: 12px/1.5 "simsun";\n' +
             '\t}\n' +
-            '\t.ui-textarea p{\n' +
+            '\t.ui-textarea p {\n' +
                 '\t\tmargin-bottom: 10px;\n' +
             '\t}\n' +
-            '\t<%- classHeaders %>{\n' +
+            '\t<%- classHeaders %> {\n' +
                 '\t\tposition: absolute;\n' +
             '\t}\n' +
             '<% _.each(blockStyles, function(blockStyle) { %>' +
-            '\t.ui-<%= blockStyle.name %>{\n' +
+            '\t.ui-<%= blockStyle.name %> {\n' +
                 '\t\twidth: <%= blockStyle.width %>px;\n' +
                 '\t\theight: <%= blockStyle.height %>px;\n' +
                 '\t\tbackground: url("images/section-<%= blockStyle.num %>.<%= blockStyle.format %>") center top no-repeat;\n' +
@@ -68,13 +68,13 @@ module.exports = {
         '</div>\n',
     styleBig:
         '<style type="text/css">\n' +
-            '\t.wid100{\n' +
+            '\t.wid100 {\n' +
                 '\t\twidth: 100%;\n' +
                 '\t\theight: auto;\n' +
                 '\t\tmargin-left: auto;\n' +
                 '\t\tmargin-right: auto;\n' +
             '\t}\n' +
-            '\t.ui-block-inner{\n' +
+            '\t.ui-block-inner {\n' +
                 '\t\twidth: <%= wrapperWidth %>px;\n' +
                 '\t\theight: auto;\n' +
                 '\t\tmargin-left: auto;\n' +
@@ -82,7 +82,7 @@ module.exports = {
                 '\t\tposition: relative;\n' +
             '\t}\n' +
             '<% _.each(blockStyles, function(blockStyle) { %>' +
-            '\t.ui-<%= blockStyle.name %>{\n' +
+            '\t.ui-<%= blockStyle.name %> {\n' +
                 '\t\theight: <%= blockStyle.height %>px;\n' +
                 '\t\tbackground: url("images/section-<%= blockStyle.num %>.<%= blockStyle.format %>") center top no-repeat;\n' +
             '\t}\n' +
@@ -95,20 +95,20 @@ module.exports = {
         '<% }); %>',
     styleBig2:
         '<style type="text/css">\n' +
-            '\t.wid100{\n' +
+            '\t.wid100 {\n' +
                 '\t\twidth: 100%;\n' +
                 '\t\theight: auto;\n' +
                 '\t\tmargin-left: auto;\n' +
                 '\t\tmargin-right: auto;\n' +
             '\t}\n' +
-            '\t.ui-block-inner{\n' +
+            '\t.ui-block-inner {\n' +
                 '\t\twidth: <%= wrapperWidth %>px;\n' +
                 '\t\theight: auto;\n' +
                 '\t\tmargin-left: auto;\n' +
                 '\t\tmargin-right: auto;\n' +
                 '\t\tposition: relative;\n' +
             '\t}\n' +
-            '\t.ui-rect{\n' +
+            '\t.ui-rect {\n' +
                 '\t\tposition: absolute;\n' +
                 '\t\tdisplay:inline-block;\n' +
                 '\t\tbackground: url("https://i.alipayobjects.com/e/201309/11htyhgbGn.gif") repeat;\n' +
@@ -117,16 +117,16 @@ module.exports = {
                 '\t\tposition: absolute;\n' +
                 '\t\tfont: 12px/1.5 "simsun";\n' +
             '\t}\n' +
-            '\t.ui-textarea p{\n' +
+            '\t.ui-textarea p {\n' +
                 '\t\tmargin-bottom: 10px;\n' +
             '\t}\n' +
             '<% _.each(blockStyles, function(blockStyle) { %>' +
-            '\t.ui-<%= blockStyle.name %>{\n' +
+            '\t.ui-<%= blockStyle.name %> {\n' +
                 '\t\theight: <%= blockStyle.height %>px;\n' +
                 '\t\tbackground: url("images/section-<%= blockStyle.num %>.<%= blockStyle.format %>") center top no-repeat;\n' +
             '\t}\n' +
             '<% _.each(blockStyle.children, function(child, childIndex) { %>' +
-            '\t.ui-<%= blockStyle.name %>-<%= childIndex + 1 %>{\n' +
+            '\t.ui-<%= blockStyle.name %>-<%= childIndex + 1 %> {\n' +
                 '\t\twidth: <%= child.width %>px;\n' +
                 '\t\theight: <%= child.height %>px;\n' +
                 '\t\tbackground: url("images/section-<%= blockStyle.num %>-<%= childIndex + 1 %>.<%= blockStyle.format %>") center top no-repeat;\n' +
