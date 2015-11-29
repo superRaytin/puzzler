3a0b9e3404e491dcc98a76c34c46c99207c42578
leon.shi <superRaytin@163.com>
2015-11-29 15:55:34 +0800

    :curly_loop:

--- src/js/textarea.js
+++ src/js/textarea.js
@@ -13,7 +13,7 @@ var window = global.window,
 
 var main = {
     // 自定义内容区
-    drawText: function(){
+    drawText: function() {
         var imgCover = $('#J-image-process-cover'),
             imgItem = imgCover.parent(),
             newTextArea = $('<div class="textzone"></div>'),
@@ -35,7 +35,7 @@ var main = {
             trueWidth, trueHeight;
 
         // 点击层或resize时，将焦点放在当前的层，同时赋值基础数据
-        var focusCurrentBlock = function(e, id){
+        var focusCurrentBlock = function(e, id) {
             main.focus(id);
             currentRect = cache.textArea[cache.focusTextAreaId];
 
@@ -49,18 +49,18 @@ var main = {
             rectAddY = top - currentRect.top;
 
             var $curRect = $('#' + id);
-            if($curRect.hasClass('textzone-error')){
+            if ($curRect.hasClass('textzone-error')) {
                 $curRect.removeClass('textzone-error');
             }
         };
 
-        imgCover.mousedown(function(e){
+        imgCover.mousedown(function(e) {
             // 右键不触发
-            if(e.button === 2) return;
+            if (e.button === 2) return;
 
-            if(imgCover.find('textarea').is(':focus')) return;
+            if (imgCover.find('textarea').is(':focus')) return;
 
-            if(cache.drawText){
+            if (cache.drawText) {
                 _drawMove = true;
                 left = e.clientX - cache.minusX + imgItem.scrollLeft();
                 top = e.clientY - cache.minusY + imgItem.scrollTop();
@@ -72,8 +72,8 @@ var main = {
             }
         });
 
-        $(document).mousemove(function(e){
-            if(_drawMove || _rectMove || _rectResize){
+        $(document).mousemove(function(e) {
+            if (_drawMove || _rectMove || _rectResize) {
                 scrollLeft = imgItem.scrollLeft();
                 scrollTop = imgItem.scrollTop();
                 distX = e.clientX - cache.minusX + scrollLeft;
@@ -85,8 +85,8 @@ var main = {
                 var $currentRect = $('#' + currentRectId);
             }
 
-            if(_drawMove){
-                if(!rectCreated){
+            if (_drawMove) {
+                if (!rectCreated) {
                     textarea = newTextArea.clone().attr('id', 'textarea-' + cache.textAreauuid);
                     imgCover.append(textarea);
                     rectCreated = true;
@@ -105,20 +105,20 @@ var main = {
 
                 _drawMoving = true;
             }
-            else if(_rectMove){
+            else if (_rectMove) {
                 trueLeft = distX - rectAddX;
                 trueTop = distY - rectAddY;
 
                 // 子欲拖出图片区域之外，我偏不让
                 // 拖到图像区域外松手时，将left值替换成边界值
-                if(trueLeft > imgWidth - currentRect.width - 2){
+                if (trueLeft > imgWidth - currentRect.width - 2) {
                     trueLeft = imgWidth - currentRect.width - 2;
-                }else if(trueLeft < 0){
+                }else if (trueLeft < 0) {
                     trueLeft = 0;
                 };
-                if(trueTop > imgHeight - currentRect.height - 2){
+                if (trueTop > imgHeight - currentRect.height - 2) {
                     trueTop = imgHeight - currentRect.height - 2;
-                }else if(trueTop < 0){
+                }else if (trueTop < 0) {
                     trueTop = 0;
                 };
 
@@ -129,19 +129,19 @@ var main = {
 
                 _rectMoving = true;
             }
-            else if(_rectResize){
+            else if (_rectResize) {
                 trueWidth = distX - currentRect.left;
                 trueHeight = distY - currentRect.top;
 
                 // 子欲拖出图片区域之外，我偏不让
-                if(trueWidth > imgWidth - currentRect.left - 2){
+                if (trueWidth > imgWidth - currentRect.left - 2) {
                     trueWidth = imgWidth - currentRect.left - 2;
-                }else if(trueWidth < 10){
+                }else if (trueWidth < 10) {
                     trueWidth = 10;
                 };
-                if(trueHeight > imgHeight - currentRect.top - 2){
+                if (trueHeight > imgHeight - currentRect.top - 2) {
                     trueHeight = imgHeight - currentRect.top - 2;
-                }else if(trueHeight < 10){
+                }else if (trueHeight < 10) {
                     trueHeight = 10;
                 };
 
@@ -151,25 +151,25 @@ var main = {
                 });
 
                 // 热区设置层同步赋值
-                /*if(!$currentRect.find('.setting-area').hasClass('hide')){
+                /*if (!$currentRect.find('.setting-area').hasClass('hide')) {
                     $currentRect.find('input[data-type="width"]').val(trueWidth);
                     $currentRect.find('input[data-type="height"]').val(trueHeight);
                 }*/
 
                 _rectResizing = true;
             }
-        }).mouseup(function(e){
-                if(_drawMove){
-                    if(_drawMoving){
-                        if(distX < left || distY < top){
+        }).mouseup(function(e) {
+                if (_drawMove) {
+                    if (_drawMoving) {
+                        if (distX < left || distY < top) {
                             main.delete(cache.focusTextAreaId);
                         }
                         else{
                             // 超出图片区域之外，将dist值替换成边界值
-                            if(distX > imgWidth - 2){
+                            if (distX > imgWidth - 2) {
                                 distX = imgWidth - 2;
                             }
-                            if(distY > imgHeight - 2){
+                            if (distY > imgHeight - 2) {
                                 distY = imgHeight - 2;
                             }
 
@@ -190,8 +190,8 @@ var main = {
                     rectCreated = false;
                 }
 
-                if(_rectMove){
-                    if(_rectMoving){
+                if (_rectMove) {
+                    if (_rectMoving) {
                         cache.textArea[currentRectId].left = trueLeft;
                         cache.textArea[currentRectId].top = trueTop;
 
@@ -200,8 +200,8 @@ var main = {
                     _rectMove = false;
                 }
 
-                if(_rectResize){
-                    if(_rectResizing){
+                if (_rectResize) {
+                    if (_rectResizing) {
                         cache.textArea[currentRectId].width = trueWidth;
                         cache.textArea[currentRectId].height = trueHeight;
 
@@ -209,16 +209,16 @@ var main = {
                     }
                     _rectResize = false;
 
-                    if(cache.drawText){
+                    if (cache.drawText) {
                         imgCover.removeClass('resizing');
                     }
                 }
             });
 
         // 文字区移动
-        imgCover.delegate('.cover', 'mousedown', function(e){
+        imgCover.delegate('.cover', 'mousedown', function(e) {
             e.stopPropagation();
-            //if(e.button === 2) return;
+            //if (e.button === 2) return;
             currentRectId = this.parentNode.id;
             focusCurrentBlock(e, currentRectId);
 
@@ -226,7 +226,7 @@ var main = {
         });
 
         // 编辑
-        imgCover.delegate('.cover', 'dblclick', function(e){
+        imgCover.delegate('.cover', 'dblclick', function(e) {
             e.stopPropagation();
             var that = $(this),
                 input = that.siblings('textarea'),
@@ -234,7 +234,7 @@ var main = {
                 edit = that.siblings('.edit_textarea'),
                 setting = that.siblings('.setting_textarea');
 
-            if(input.hasClass('hide')){
+            if (input.hasClass('hide')) {
                 that.addClass('hide');
                 input.removeClass('hide').focus();
                 preview_frame.addClass('hide');
@@ -247,22 +247,22 @@ var main = {
         });
 
         // 失去焦点加上遮罩
-        imgCover.delegate('textarea', 'blur', function(e){
+        imgCover.delegate('textarea', 'blur', function(e) {
             e.stopPropagation();
             $(this).siblings('.cover').removeClass('hide');
-        }).delegate('textarea', 'change', function(e){
+        }).delegate('textarea', 'change', function(e) {
                 var that = $(this);
                 cache.textArea[that.parent().attr('id')].originContent = that.val();
                 cache.textArea[that.parent().attr('id')].content = main.parseMarkdown(that.val());
             });
 
         // 文字区收缩
-        imgCover.delegate('.resize_textarea', 'mousedown', function(e){
+        imgCover.delegate('.resize_textarea', 'mousedown', function(e) {
             e.stopPropagation();
             currentRectId = this.parentNode.id;
             focusCurrentBlock(e, currentRectId);
 
-            if(cache.drawText){
+            if (cache.drawText) {
                 imgCover.addClass('resizing');
             }
 
@@ -270,20 +270,20 @@ var main = {
         });
 
         // 文字区设置
-        imgCover.delegate('.setting_textarea', 'mousedown', function(e){
+        imgCover.delegate('.setting_textarea', 'mousedown', function(e) {
             e.stopPropagation();
             console.log('setting textzone');
             main.preview($(this), 'setting');
-        }).delegate('.edit_textarea', 'mousedown', function(e){
+        }).delegate('.edit_textarea', 'mousedown', function(e) {
                 e.stopPropagation();
                 console.log('edit textzone');
                 main.preview($(this), 'edit');
             });
     },
-    parseMarkdown: function(content, res){
-        res = '';
-        window.marked(content, function(err, parsedData){
-            if(err){
+    parseMarkdown: function(content) {
+        var res = '';
+        window.marked(content, function(err, parsedData) {
+            if (err) {
                 return alertify.error('markdown解析出错，请检查', 10000);
             }
             res = parsedData;
@@ -291,13 +291,13 @@ var main = {
 
         return res;
     },
-    preview: function(current, type){
+    preview: function(current, type) {
         var textzone = $('#' + cache.focusTextAreaId),
             mode = type, that;
 
-        if(!current){
-            if(!cache.focusTextAreaId) return;
-            if(textzone.find('.setting_textarea').hasClass('hide')){
+        if (!current) {
+            if (!cache.focusTextAreaId) return;
+            if (textzone.find('.setting_textarea').hasClass('hide')) {
                 that = textzone.find('.edit_textarea');
                 mode = 'edit';
             }else{
@@ -315,7 +315,7 @@ var main = {
             frame = preview_frame.find('iframe');
 
         // 预览
-        if(mode === 'setting'){
+        if (mode === 'setting') {
             that.addClass('hide');
             input.addClass('hide');
 
@@ -325,7 +325,7 @@ var main = {
             frame.contents().find('body').html(main.parseMarkdown(value));
         }
         // 编辑
-        else if(mode === 'edit'){
+        else if (mode === 'edit') {
             that.addClass('hide');
             input.removeClass('hide');
 
@@ -334,7 +334,7 @@ var main = {
         }
 
     },
-    focus: function(textAreaId){
+    focus: function(textAreaId) {
         var imgCover = $('#J-image-process-cover');
 
         cache.focusTextAreaId = textAreaId;
@@ -345,7 +345,7 @@ var main = {
         imgCover.find('.textzone').removeClass('textzone-focus');
         $('#' + textAreaId).addClass('textzone-focus');
     },
-    reset: function(){
+    reset: function() {
         $('#J-image-process-cover').find('.textzone').remove();
 
         $.extend(true, mass.cache, {
@@ -366,7 +366,7 @@ var main = {
      *     content: String
      * }
      * */
-    add: function(option){
+    add: function(option) {
         var textAreauuid = cache.textAreauuid,
             rectId = 'textarea-' + textAreauuid,
             imgCover = $('#J-image-process-cover'),
@@ -390,7 +390,7 @@ var main = {
         cache.textAreaNum++;
         cache.textArea[rectId] = option;
     },
-    delete: function(textAreaId){
+    delete: function(textAreaId) {
         var curTextArea = $('#' + textAreaId);
 
         cache.textAreaNum--;
@@ -401,17 +401,17 @@ var main = {
         curTextArea.remove();
     },
     // 批量导入
-    import: function(options, callback){
-        _.each(options, function(item){
+    import: function(options, callback) {
+        _.each(options, function(item) {
             main.add(item);
         });
 
-        if(callback){
+        if (callback) {
             callback();
         }
     },
     // 检查文字区位置
-    check: function(){
+    check: function() {
         var lines = cache.line,
             rects = cache.textArea,
             isBig2 = !!(cache.img.width > 990 && cache.lineY > 1),
@@ -423,20 +423,20 @@ var main = {
             blocks,
             res = true;
 
-        if(!cache.textAreaNum) return res;
+        if (!cache.textAreaNum) return res;
 
         $('.textzone').removeClass('textzone-error');
 
-        $.each(rects, function(rectId, rect){
+        $.each(rects, function(rectId, rect) {
             critical.Y.x = rect.left;
             critical.Y.y = rect.left + rect.width;
             critical.X.x = rect.top;
             critical.X.y = rect.top + rect.height;
-            $.each(lines, function(lineId, line){
+            $.each(lines, function(lineId, line) {
                 var type = line.type,
                     pos = line.pos;
 
-                if(pos > critical[type].x && pos < critical[type].y){
+                if (pos > critical[type].x && pos < critical[type].y) {
                     res = false;
                     console.log(rectId, type, critical);
                     console.log(lineId, pos);
@@ -446,17 +446,17 @@ var main = {
             });
         });
 
-        if(res){
-            if(isBig2){
+        if (res) {
+            if (isBig2) {
                 blocks = mass.getCutBlocks('children');
 
-                $.each(blocks, function(i, block){
+                $.each(blocks, function(i, block) {
                     var blockParentIndex = block.parentBlockIndex;
-                    $.each(rects, function(rectId, rect){
+                    $.each(rects, function(rectId, rect) {
                         var topAndSelfHeight = block.y + block.height,
                             leftAndSelfWidth = block.x + block.width;
 
-                        if(block.width > rect.width && block.x < rect.left && block.y < rect.top && (topAndSelfHeight > rect.top + rect.height) && (leftAndSelfWidth > rect.left + rect.width)){
+                        if (block.width > rect.width && block.x < rect.left && block.y < rect.top && (topAndSelfHeight > rect.top + rect.height) && (leftAndSelfWidth > rect.left + rect.width)) {
                             rectInBlock[blockParentIndex] = rectInBlock[blockParentIndex] || [];
                             rectInBlock[blockParentIndex].push({
                                 rect: rect,
@@ -471,12 +471,12 @@ var main = {
             else{
                 blocks = mass.getCutBlocks();
 
-                $.each(blocks, function(i, block){
-                    $.each(rects, function(rectId, rect){
+                $.each(blocks, function(i, block) {
+                    $.each(rects, function(rectId, rect) {
                         var topAndSelfHeight = block.y + block.height,
                             leftAndSelfWidth = block.x + block.width;
 
-                        if(block.width > rect.width && block.x < rect.left && block.y < rect.top && (topAndSelfHeight > rect.top + rect.height) && (leftAndSelfWidth > rect.left + rect.width)){
+                        if (block.width > rect.width && block.x < rect.left && block.y < rect.top && (topAndSelfHeight > rect.top + rect.height) && (leftAndSelfWidth > rect.left + rect.width)) {
                             rectInBlock[i] = rectInBlock[i] || [];
                             rectInBlock[i].push({
                                 rect: rect,
@@ -495,10 +495,11 @@ var main = {
         //return false;
     },
     // 查看HTML源代码
-    previewHTML: function(){
+    previewHTML: function() {
         var currentTextarea = $('#' + cache.focusTextAreaId),
             value = currentTextarea.find('textarea').val();
         mass.dialog(main.parseMarkdown(value).replace(/</mg, '&lt;').replace(/>/mg, '&gt;'), true);
     }
 };
+
 module.exports = main;
